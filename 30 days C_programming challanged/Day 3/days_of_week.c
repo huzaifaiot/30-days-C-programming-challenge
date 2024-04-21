fab894c75eb86b3026828c87bcaf7eba9d654f85
Huzaifa Saim <162149728+huzaifaiot@users.noreply.github.com>
2024-04-21 14:28:53 +0500

    Add files via upload

---
+++ 30 days C_programming challanged/Day 3/days_of_week.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// Define constant for the number of days in a week
+#define DAYS_IN_WEEK 7
+
+int main() {
+    // Array to store the names of the days
+    char *days[DAYS_IN_WEEK] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    
+    // Display each day's name
+    printf("Days of the week:\n");
+    for (int i = 0; i < DAYS_IN_WEEK; i++) {
+        printf("%d. %s\n", i + 1, days[i]);
+    }
+    
+    return 0;
+}
